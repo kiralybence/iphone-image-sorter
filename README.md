0e8f7e9f1c835e102c8e0ce297b707fc932c3f39
Bence Király <kiralybencepecs@gmail.com>
2022-09-06 02:04:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# iPhone image sorter
+
+A quick and easy way to sort your photos imported from an iPhone.
+
+It will move your photos to subdirectories (DCIM, Screenshots, Downloads) based on their filenames.
+
+## How to use
+
+You must specify the path where the images are located at.
+
+```bash
+node index.js <path>
+```
+
+The path must be an absolute path.
+
+```bash
+# This will NOT work
+node index.js ./images
+```
+
+On Windows, you cannot use single backslashes in the path.
+
+```bash
+# This will NOT work
+node index.js D:\Backup\Images
+
+# You have to use either double backslashes
+node index.js D:\\Backup\\Images
+
+# or single forward slashes
+node index.js D:/Backup/Images
+```
---
+++ index.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const path = require('path');
+
+const mainPath = process.argv[2];
+
+if (!mainPath) {
+    throw 'Path is not given.';
+}
+
+if (!path.isAbsolute(mainPath)) {
+    throw 'Path is not absolute.';
+}
+
+if (!fs.existsSync(mainPath)) {
+    throw 'Directory does not exist.';
+}
+
+console.log(`Finding images in: ${mainPath}`);
+
+let files = fs.readdirSync(mainPath, { withFileTypes: true })
+    .filter(dirent => dirent.isFile())
+    .map(file => file.name);
+
+console.log(`Found ${files.length} images.`);
+
+// TODO: make everything async
+files.forEach(file => {
+    file = path.parse(file);
+
+    if (file.name.startsWith('IMG_')) {
+        // If the rest of the filename are numbers only
+        // Some non-iPhone images start with "IMG_", but end with a datetime
+        if (/^[0-9]+$/.test(file.name.substring(4))) {
+            switch (file.ext) {
+                case '.PNG':
+                    move(file, 'Screenshots');
+                    break;
+
+                case '.JPG':
+                case '.HEIC':
+                case '.MOV':
+                case '.MP4':
+                    move(file, 'DCIM');
+                    break;
+
+                default:
+                    move(file, 'Downloads');
+                    break;
+            }
+        } else {
+            move(file, 'Downloads');
+        }
+    } else {
+        move(file, 'Downloads');
+    }
+});
+
+/**
+ * Move a file to a subdirectory.
+ * 
+ * @param {path.ParsedPath} file The file object
+ * @param {string} targetDir The target subdirectory
+ */
+function move(file, targetDir) {
+    let absPath = path.join(mainPath, targetDir);
+    
+    if (!fs.existsSync(absPath)) {
+        fs.mkdirSync(absPath);
+    }
+
+    console.log(`Moving: ${file.base} -> ${targetDir}`);
+
+    fs.renameSync(
+        path.join(mainPath, file.base),
+        path.join(mainPath, targetDir, file.base)
+    );
+}
